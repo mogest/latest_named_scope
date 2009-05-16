4ec7259bf1e1c2a164e1a1ab49e5b30f96721d54
Roger Nesbitt <roger@seriousorange.com>
2009-05-16 15:17:57 +0100

    Completely rewrite it; now named_scope is defined on child, not parent

--- lib/latest_named_scope.rb
+++ lib/latest_named_scope.rb
@@ -7,43 +7,44 @@ module IncludeLatestNamedScope
 
   module ClassMethods
     #
-    # Defines a named_scope that will include the specified has_many association and preload only
-    # the most recent child record.
+    # Defines a named_scope that, when used, will only load the latest record for each parent key.
+    # By default, the latest record is the one with the latest created_at time, but that can be changed.
     #
-    # The last argument can be a hash with the following options:
+    # Note!  Currently, if there are two records with the same created_at time, they will both be returned.
+    # This should probably be fixed.
+    #
+    # You must pass the parent key.  All other keys are optional.
+    #   :parent => :some_model specifies the parent model
     #   :order => :some_field  use a field other than +created_at+ to find the latest record
-    #   :name => :scope_name   create a scope name other than "include_latest_" followed by the association's name in the singular
+    #   :name => :scope_name   name of the scope.  By default it is "latest_only"
     #
     # Sample usage:
     #
-    # class Post < ActiveRecord::Base
-    #   has_many :comments
-    #   include_latest_named_scope :comments
-    #
-    #   def self.last_comment_from_all_posts
-    #     include_latest_comment.all.collect(&:comments).flatten
-    #   end
+    # class Comment < ActiveRecord::Base
+    #   belongs_to :post
+    #   latest_named_scope :parent => :post
     # end
     #
-    def include_latest_named_scope(*args)
-      opts = args.last.is_a?(Hash) ? args.pop : {}
+    # >> Comment.all.collect {|c| [c.post_id, c.created_at.to_s(&:db)]}
+    # => [[1, "2007-11-22 11:51:54"], [1, "2007-11-22 12:25:30"], [2, "2007-11-22 21:12:05"]]
+    #
+    # >> Comment.latest_only.all.collect {|c| [c.post_id, c.created_at.to_s(&:db)]}
+    # => [[1, "2007-11-22 12:25:30"], [2, "2007-11-22 21:12:05"]]
+    #
+    def latest_named_scope(opts)
+      raise ArgumentError, "must specify a parent association that this model belongs_to" unless opts[:parent]
+      parent = opts[:parent]
       order = opts[:order] || "created_at"
 
-      args.each do |association|
-        single_child_name = association.to_s.singularize
-        child_model = single_child_name.camelize.constantize
-        parent_key = connection.quote_column_name(child_model.reflect_on_association(name.underscore.to_sym).association_foreign_key)
+      parent_key = connection.quote_column_name(reflect_on_association(parent).association_foreign_key)
+      table_name = quoted_table_name
+      duplicate = connection.quote_table_name("#{name.underscore}_duplicate")
 
-        table_name = child_model.quoted_table_name
-        duplicate = connection.quote_table_name("#{association}_duplicate")
-
-        scope_name = opts[:name] || "include_latest_#{single_child_name}"
-        
-        named_scope scope_name,
-          :include => association,
-          :joins => "LEFT JOIN #{table_name} #{duplicate} ON (#{table_name}.#{order} < #{duplicate}.#{order} AND #{table_name}.#{parent_key} = #{duplicate}.#{parent_key})",
-          :conditions => "(1 = 1 OR #{table_name}.id IS NOT NULL) AND #{duplicate}.#{primary_key} IS null"
-      end
+      scope_name = opts[:name] || "latest_only"
+      
+      named_scope scope_name,      
+        :joins => "LEFT JOIN #{table_name} #{duplicate} ON (#{table_name}.#{order} < #{duplicate}.#{order} AND #{table_name}.#{parent_key} = #{duplicate}.#{parent_key})",
+        :conditions => "#{duplicate}.#{primary_key} IS null"
     end
   end
 end
